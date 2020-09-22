ef7df28665f72e5cfcb7855aa7f9c37b3b0bda6d
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-09-22 09:08:03 +0700

    :zap: Test db connection

--- application/config/database.php
+++ application/config/database.php
@@ -76,10 +76,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => getenv(DB_HOST),
-	'username' => getenv(DB_USER),
-	'password' => getenv(DB_PASS),
-	'database' => getenv(DB_NAME),
+	'hostname' => getenv('DB_HOST'),
+	'username' => getenv('DB_USER'),
+	'password' => getenv('DB_PASS'),
+	'database' => getenv('DB_NAME'),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
